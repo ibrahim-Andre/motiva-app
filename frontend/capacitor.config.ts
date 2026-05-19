99f571c243e054f48bd84864e278efa2be2e1ddd
ibrahim Tanriover <ibrahimtanriover64@gmail.com>
2026-05-20 01:41:14 +0200

    ios app setup7

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,11 +1,6 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.taxi.app',
-  appName: 'Taxi',
-  webDir: 'build'
-};
-const config: CapacitorConfig = {
   appId: 'com.ibrahim.motiva',
   appName: 'Motiva',
   webDir: 'build',
